b8204b62577eb5b21e97998ea8dad5314306087e
jhonny-berdeja <jhonny.berdeja.100@gmail.com>
2024-11-25 01:04:11 -0300

    clases, objetos y herencia

--- index.ts
+++ index.ts
@@ -1,2 +1,54 @@
 console.log("Hola");
-//For ecxecute ts-node index.ts 
+//For ecxecute ts-node index.ts
+
+
+//Tipos de datos
+let nombre: string | undefined = 'Juan';
+
+let numero: number = 123;
+let booleano: boolean = true;
+let nulo: null =null;
+let indefinido: undefined = undefined;
+let arreglo: number[] = [1, 2, 3];
+
+//Inferencia de tipos
+const arreglo2 = [1,2, true, 3, "H", 4];
+arreglo2.push(1);
+arreglo2.push(false);
+arreglo2.push("A");
+
+//Objeto
+
+class Persona{
+    nombre: string;
+    edad: number;
+
+    constructor(nombre: string, edad: number){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    saludar(): void {
+        console.log(`Hola ${this.nombre}`);
+    }
+}
+
+const persona = new Persona("Jhonny", 34);
+console.log(persona.edad);
+persona.saludar();
+
+class Estudiante extends Persona {
+    curso: string;
+    constructor(nombre: string, edad: number, curso: string){
+        super(nombre, edad);
+        this.curso = curso;
+    }
+    estudia(): void {
+        console.log(`${this.nombre} está estudiando ${this.curso}`);
+    }
+}
+
+const estudiante = new Estudiante("Milton", 34, "Matematicas");
+
+estudiante.estudia();
+//For ecxecute ts-node index.ts
